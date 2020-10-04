f7cca0047be56ac764560526bc8383068939d15e
mentayk <kmentay@gmail.com>
2020-10-03 20:34:26 -0600

    made new counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counters, setCounters] = useState([1, 2, 3, 4, 5]);
+    const addCounter = () => {
+        const newCounter = [...counters]
+        newCounter.push(0);
+        setCounters(newCounter)
+    }
+    const deleteCounter = (index) => {
+        const newCounter = counters.filter((el, i) =>
+            i !== index)
+        setCounters(newCounter)
+    }
+
+    const counterChange = (index, value) => {
+        const newCounter = counters.map((el, i) => {
+                if (i === index) return el + value
+                return el
+            }
+        )
+        setCounters(newCounter)
+    }
+
+    return (
+        <div className="App">
+            <button onClick={() => addCounter()}> add counter</button>
+            {counters.map((el, i) => <div>
+                <button onClick={() => counterChange(i, 1)}> +</button>
+                {el}
+                <button onClick={() => counterChange(i, -1)}> -</button>
+                <button onClick={() => deleteCounter(i)}> delete</button>
+            </div>)}
+        </div>
+    );
 }
 
 export default App;
